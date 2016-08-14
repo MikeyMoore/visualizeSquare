3ad381ae52b636c74d20d94b1fe5a7521fed9d89
MikeyMoore <icarusjedi@gmail.com>
2016-08-14 12:18:54 -0700

    Add helper method for correct square color

--- visualizeSquare/app/helpers/quiz_helper.rb
+++ visualizeSquare/app/helpers/quiz_helper.rb
@@ -1,2 +1,17 @@
 module QuizHelper
+	def squareAnswerWhite(square)
+		if square.color == 'white'
+			return 'correct'
+		else
+			return 'incorrect'
+		end
+	end
+
+	def squareAnswerBlack(square)
+		if square.color == 'black'
+			return 'correct'
+		else
+			return 'incorrect'
+		end
+	end
 end
