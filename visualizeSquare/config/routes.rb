e03eba448870f346abba67b1784a2fa444c3eb18
MikeyMoore <icarusjedi@gmail.com>
2016-09-27 17:44:07 -0700

    Move results to separate page

--- visualizeSquare/config/routes.rb
+++ visualizeSquare/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # get 'quiz/index'
-  resources :quizes
+  resources :quiz
 
   root 'quiz#index'
   # The priority is based upon order of creation: first created -> highest priority.
