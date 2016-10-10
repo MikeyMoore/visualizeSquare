a6a7d6e2f80e8cce846ea5063e2890fd55b55c44
MikeyMoore <icarusjedi@gmail.com>
2016-10-09 20:20:15 -0700

    Add users to database

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  # def create
+  #   # user = User.
+  #   session[:user_id] = user.id
+  #   redirect_to root_path
+  # end
+  #
+  # def show
+  #   @user = User.find(params[:user_id])
+  # end
+  #
+  # def destroy
+  #   session[:user_id] = nil
+  #   redirect_to root_path
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   # get 'quiz/index'
   resources :quiz
 
